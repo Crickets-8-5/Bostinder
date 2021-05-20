55d9d445e4328ca6e7015f18df1b0598626d2b00
ACER <ACER@192.168.1.104>
2021-05-19 22:59:06 -0500

    cambios panel inicio sesion

--- TINDER/src/co/edu/unbosque/view/PanelInicioSesion.java
+++ TINDER/src/co/edu/unbosque/view/PanelInicioSesion.java
@@ -6,13 +6,16 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 public class PanelInicioSesion extends JPanel {
 
-	private JLabel e_titulo;
-	private JButton b_admin, b_registrar, b_inisesion;
-	
+	private JLabel e_titulo, e_usuario, e_contra;
+	private JButton b_ingresar, b_volver;
+	private JTextField jusuario;
+	private JPasswordField jcontra;
 
 	public PanelInicioSesion() {
 		setLayout(null);
@@ -28,15 +31,31 @@ public class PanelInicioSesion extends JPanel {
 		e_titulo.setFont(new Font("Algerian", 20, 35));
 		e_titulo.setBounds(120, 60, 264, 45);
 		add(e_titulo);
+		
+		e_usuario = new JLabel("Usuario:");
+		e_usuario.setForeground(Color.WHITE);
+		e_usuario.setBounds(150, 130, 50, 25);
+		add(e_usuario);
+		
+		e_contra = new JLabel("Contraseña:");
+		e_contra.setForeground(Color.WHITE);
+		e_contra.setBounds(130, 170, 80, 25);
+		add(e_contra);
+		
+		jusuario = new JTextField();
+		jusuario.setBounds(220, 130, 180, 25);
+		add(jusuario);
+		
+		jcontra = new JPasswordField();
+		jcontra.setBounds(220, 170, 180, 25);
+		add(jcontra);
 
-		b_inisesion = new JButton("Iniciar Sesion");
-		buttons(b_inisesion, "INISESION", 160, 130, 155, 30);
+		b_ingresar = new JButton("Iniciar Sesion");
+		buttons(b_ingresar, "INISESION", 160, 230, 155, 30);
 		
-		b_registrar = new JButton("Registrarse");
-		buttons(b_registrar, "REGISTRAR", 160, 170, 155, 30);
+		b_volver = new JButton("Volver");
+		buttons(b_volver, "VOLVER", 400, 325, 70, 20);
 		
-		b_admin = new JButton("Admin");
-		buttons(b_admin, "INIADMIN", 160, 210, 155, 30);
 	}
 	
 	private void buttons(JButton button, String action,int x, int y, int w, int h) {
@@ -45,6 +64,8 @@ public class PanelInicioSesion extends JPanel {
 		button.setBackground(Color.lightGray);
 		add(button);
 	}
+	
+	
 
 	public JLabel getE_titulo() {
 		return e_titulo;
@@ -55,27 +76,19 @@ public class PanelInicioSesion extends JPanel {
 	}
 
 	public JButton getB_admin() {
-		return b_admin;
+		return b_ingresar;
 	}
 
 	public void setB_admin(JButton b_admin) {
-		this.b_admin = b_admin;
+		this.b_ingresar = b_admin;
 	}
 
 	public JButton getB_registrar() {
-		return b_registrar;
+		return b_volver;
 	}
 
 	public void setB_registrar(JButton b_registrar) {
-		this.b_registrar = b_registrar;
+		this.b_volver = b_registrar;
 	}
 
-	public JButton getB_inisesion() {
-		return b_inisesion;
-	}
-
-	public void setB_inisesion(JButton b_inisesion) {
-		this.b_inisesion = b_inisesion;
-	}
-	
 }
